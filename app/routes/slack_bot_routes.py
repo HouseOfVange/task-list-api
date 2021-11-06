5cd033d7529d1cd8b9aa27a73cae29dd3094e8b1
Vange Spracklin <houseofvange@gmail.com>
2021-11-06 15:38:42 -0700

    creates goal model and doc string comment for slack_message function

--- app/routes/slack_bot_routes.py
+++ app/routes/slack_bot_routes.py
@@ -5,11 +5,17 @@ from flask import jsonify
 
 load_dotenv()
 
-def slack_message(text):
+def slack_message(message):
+    '''sends an HTTP POST request to a slack bot. Uses
+    the Python package "requests" to send the request.
+    Function takes in message as a parameter which will be 
+    a string and the body of the message posted by the bot'''
+
     url = 'https://slack.com/api/chat.postMessage'
+
     required_params = {
         'channel': 'task-notifications', 
-        'text': text
+        'text': message
     }
     headers = {
         'Authorization': f'{os.environ.get("SLACK_BOT_TOKEN")}'
